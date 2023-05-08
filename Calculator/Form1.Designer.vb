44165ea9792bd9cb29ad518af43ce8012372a659
Ayush Kumar Jha <ayush91176@gmail.com>
2023-05-08 23:32:33 +0530

    first commit

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox1.Location = New Point(13, 33)
+        TextBox1.Margin = New Padding(4, 3, 4, 3)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(273, 38)
+        TextBox1.TabIndex = 0
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Red
+        Button1.ForeColor = SystemColors.ButtonFace
+        Button1.Location = New Point(300, 35)
+        Button1.Margin = New Padding(4, 3, 4, 3)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(74, 40)
+        Button1.TabIndex = 1
+        Button1.Text = "C"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(0), CByte(1))
+        Button2.Location = New Point(13, 105)
+        Button2.Margin = New Padding(4, 3, 4, 3)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(74, 40)
+        Button2.TabIndex = 2
+        Button2.Text = "1"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(0), CByte(2))
+        Button3.Location = New Point(107, 105)
+        Button3.Margin = New Padding(4, 3, 4, 3)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(74, 40)
+        Button3.TabIndex = 3
+        Button3.Text = "2"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.ForeColor = SystemColors.ActiveCaptionText
+        Button4.Location = New Point(201, 105)
+        Button4.Margin = New Padding(4, 3, 4, 3)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(74, 40)
+        Button4.TabIndex = 4
+        Button4.Text = "3"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.ForeColor = SystemColors.ActiveCaptionText
+        Button5.Location = New Point(13, 173)
+        Button5.Margin = New Padding(4, 3, 4, 3)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(74, 40)
+        Button5.TabIndex = 5
+        Button5.Text = "4"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.ForeColor = SystemColors.ActiveCaptionText
+        Button6.Location = New Point(107, 173)
+        Button6.Margin = New Padding(4, 3, 4, 3)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(74, 40)
+        Button6.TabIndex = 6
+        Button6.Text = "5"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.ForeColor = SystemColors.ActiveCaptionText
+        Button7.Location = New Point(201, 173)
+        Button7.Margin = New Padding(4, 3, 4, 3)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(74, 40)
+        Button7.TabIndex = 7
+        Button7.Text = "6"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.ForeColor = SystemColors.ActiveCaptionText
+        Button8.Location = New Point(13, 233)
+        Button8.Margin = New Padding(4, 3, 4, 3)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(74, 40)
+        Button8.TabIndex = 8
+        Button8.Text = "7"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.ForeColor = SystemColors.ActiveCaptionText
+        Button9.Location = New Point(107, 233)
+        Button9.Margin = New Padding(4, 3, 4, 3)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(74, 40)
+        Button9.TabIndex = 9
+        Button9.Text = "8"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.ForeColor = SystemColors.ActiveCaptionText
+        Button10.Location = New Point(201, 233)
+        Button10.Margin = New Padding(4, 3, 4, 3)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(74, 40)
+        Button10.TabIndex = 10
+        Button10.Text = "9"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.ForeColor = SystemColors.ActiveCaptionText
+        Button11.Location = New Point(13, 295)
+        Button11.Margin = New Padding(4, 3, 4, 3)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(74, 40)
+        Button11.TabIndex = 11
+        Button11.Text = "0"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.ForeColor = SystemColors.ActiveCaptionText
+        Button12.Location = New Point(107, 295)
+        Button12.Margin = New Padding(4, 3, 4, 3)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(74, 40)
+        Button12.TabIndex = 12
+        Button12.Text = "."
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = Color.ForestGreen
+        Button13.ForeColor = SystemColors.ButtonFace
+        Button13.Location = New Point(201, 295)
+        Button13.Margin = New Padding(4, 3, 4, 3)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(74, 40)
+        Button13.TabIndex = 13
+        Button13.Text = "="
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.ForeColor = SystemColors.ActiveCaptionText
+        Button14.Location = New Point(300, 105)
+        Button14.Margin = New Padding(4, 3, 4, 3)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(74, 40)
+        Button14.TabIndex = 14
+        Button14.Text = "/"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Button15
+        ' 
+        Button15.ForeColor = SystemColors.ActiveCaptionText
+        Button15.Location = New Point(300, 173)
+        Button15.Margin = New Padding(4, 3, 4, 3)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(74, 40)
+        Button15.TabIndex = 15
+        Button15.Text = "*"
+        Button15.UseVisualStyleBackColor = True
+        ' 
+        ' Button16
+        ' 
+        Button16.ForeColor = SystemColors.ActiveCaptionText
+        Button16.Location = New Point(300, 233)
+        Button16.Margin = New Padding(4, 3, 4, 3)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(74, 40)
+        Button16.TabIndex = 16
+        Button16.Text = "-"
+        Button16.UseVisualStyleBackColor = True
+        ' 
+        ' Button17
+        ' 
+        Button17.ForeColor = SystemColors.ActiveCaptionText
+        Button17.Location = New Point(300, 295)
+        Button17.Margin = New Padding(4, 3, 4, 3)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(74, 40)
+        Button17.TabIndex = 17
+        Button17.Text = "+"
+        Button17.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Arial Rounded MT Bold", 18F, FontStyle.Italic, GraphicsUnit.Point)
+        Label1.ForeColor = SystemColors.ActiveCaptionText
+        Label1.Location = New Point(128, 9)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 34)
+        Label1.TabIndex = 18
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(255), CByte(255), CByte(128))
+        BackgroundImage = My.Resources.Resources.hulk__2_
+        BackgroundImageLayout = ImageLayout.Center
+        ClientSize = New Size(397, 357)
+        Controls.Add(Label1)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Font = New Font("Rockwell", 10.2F, FontStyle.Regular, GraphicsUnit.Point)
+        ForeColor = SystemColors.ControlLight
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "Form1"
+        Text = "NS Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Label1 As Label
+End Class
